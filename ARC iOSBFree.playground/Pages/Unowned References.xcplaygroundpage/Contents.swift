27272ed602b1e2f666736c47fe5ba0e0e0fc23b5
Brayton-Lordianto <braylord30@gmail.com>
2023-03-18 22:34:19 -0400

    finishing comments on using unowned wrongly

--- ARC iOSBFree.playground/Pages/Unowned References.xcplaygroundpage/Contents.swift
+++ ARC iOSBFree.playground/Pages/Unowned References.xcplaygroundpage/Contents.swift
@@ -11,7 +11,9 @@ class A {
 // unowned means it is not owned by anyone, and therefore doesn't add to the counter system
 // in weak you have the safety of everything that is referencing the weak reference will go to nil, which is intended
 // but if you don't want the references to be nil but you want the thing to be released from memory
-// 2.29
+// this will give SIGABRT error because basically the memory address is not owned so no permissions to overwrite
+// ALSO, still gets deallocated since we are not increasing the counter, so immediately deallocated
+// but the address still exists in the stack
 unowned var a: A? = A()
 print("a is \(a)") // prints a is nil
 
